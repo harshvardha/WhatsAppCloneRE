99bc9d16f70ddb8044d52097a970a1b7f4185147
harshvardhan singh <harshvardhansingh458@protonmail.com>
2023-03-03 00:15:08 +0530

    added background chat image

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,6 @@
+export const ICON_PDF = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/27_Pdf_File_Type_Adobe_logo_logos-512.png';
+export const DEFAULT_PROFILE_PICTURE = 'https://static.straitstimes.com.sg/s3fs-public/articles/2020/12/01/af_moneyheist_011220.jpg';
+export const EMPTY_CHAT_IMAGE = 'https://i.gadgets360cdn.com/large/whatsapp_multi_device_support_update_image_1636207150180.jpg';
+export const QR_CODE_IMAGE = 'https://www.ginifab.com/feeds/qr_code/img/qrcode.jpg';
+export const EMPTY_PROFILE_PICTURE = 'https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.png';
+export const CHAT_BACKGROUND_IMAGE = 'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png';
